6cb37feb236f706489e280328dcacfe0327771b2
isnendyankp <isnendyankp@gmail.com>
2023-06-24 15:11:15 +0700

    S20-194:  add ternary operator

--- script.js
+++ script.js
@@ -8,17 +8,16 @@ function changeBackground(number) {
     }
     // Reset background
     body.className = '';
+    /// If background already on, turn off, else turn on background
+    // add ternary operator
     switch (number) {
-    case '1':
-      body.classList.add('background-1');
-      break;
-    case '2':
-      body.classList.add('background-2');
-      break;
-    case '3':
-      body.classList.add('background-3');
-      break;
-    default:
-      break;
-  }
+        case '1':
+        return (previousBackground === 'background-1' ? false : body.classList.add('background-1'));
+        case '2':
+        return (previousBackground === 'background-2' ? false : body.classList.add('background-2'));
+        case '3':
+        return (previousBackground === 'background-3' ? false : body.classList.add('background-3'));
+        default:
+        break;
+    }
 }
